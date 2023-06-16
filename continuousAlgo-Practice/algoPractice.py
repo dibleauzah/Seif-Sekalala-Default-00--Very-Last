4d1c0029f291d08fc932bac772830aeda51962c0
Seif Sekalala <dibleauzah@gmail.com>
2023-06-16 11:18:49 -0400

    Day 2--June 16 (2023)--of algo practice.

---
+++ continuousAlgo-Practice/algoPractice.py
@@ -0,0 +1,22 @@
+
+#!_Day 2 of algo-practice: June 16 (2023)--Python Version
+
+# def fizzBuzz(n):
+
+#     result = []
+#     for i in range(1, n + 1):
+#         add = ''
+#         if i % 3 == 0:
+#             add += 'Fizz'
+#         if i % 5 == 0:
+#             add += 'Buzz'
+#         if add == '':
+#             result.append(i)
+#         else:
+#             result.append(add)
+#     return result
+
+# print (fizzBuzz(10))
+
+#?_Test result successful; see below.
+#?_Result: [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz']
--- continuousAlgo-Practice/dist/algoPractice.dev.js
+++ continuousAlgo-Practice/dist/algoPractice.dev.js
@@ -1,21 +1,16 @@
 //!_Day 2 of algo-practice: June 16 (2023)--JS Version.
-
 // let myArray = [1, 2, 3, 4, 5];
-
 // var min = myArray[0];
 // var max = myArray[0];
 // var avg = 0;
-
 // for (let i = 0; i < myArray.length; i++){
 // 	if (myArray[i] < min) min = myArray[i];
 // 	if (myArray[i] > max) max = myArray[i];
 // 	avg += myArray[i];
 // }
-
 // avg /= myArray.length;
-
 // console.log("Hi! Please note that your min-# is : " + min);
 // console.log(" ; in addition, your max-# is : " + max);
 // console.log(" ; and finally, your average is : " + avg + " Thanks for visiting. Have an awesome day! :-)");
-
 //?_Test successful.
+"use strict";

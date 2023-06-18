c9e0caa599c137bfd15e58ecfbbe8a4faa5d4837
Seif Sekalala <dibleauzah@gmail.com>
2023-06-18 18:11:45 -0400

    Day 3--June 18, (2023)--of algo practice.

--- continuousAlgo-Practice/dist/algoPractice.dev.js
+++ continuousAlgo-Practice/dist/algoPractice.dev.js
@@ -1,4 +1,18 @@
-//!_Day 2 of algo-practice: June 16 (2023)--JS Version.
+//?_Template:
+
+/*
+?_Day X: [Add Date Here] // [And if multiple sessions are done in a day, add session # here.]
+?_Goals/"parameters" for this day/session's practice.
+
+?_Index-#--e.g., 1 of day's algo:
+
+?_Line 0 of algo 1
+?_Line 1 of algo 1 
+?_Line 2 of algo 1
+?_...
+?_Console.log ([algoName]);
+*/
+//!_Day 2 of Algo-Practice: June 16 (2023)--JS Version.
 // let myArray = [1, 2, 3, 4, 5];
 // var min = myArray[0];
 // var max = myArray[0];
@@ -13,4 +27,14 @@
 // console.log(" ; in addition, your max-# is : " + max);
 // console.log(" ; and finally, your average is : " + avg + " Thanks for visiting. Have an awesome day! :-)");
 //?_Test successful.
+//*-------------------------------------//
+//!_Day 2 of Algo-Practice: June 16 (2023)--JS Version.
+
+/*
+?_Day 3: June 18, 2023 
+?_Goals/"parameters" for this day/session's practice: 
+*_>> Simply for re-acclimation purposes. Meaning, for me to re-acclimate myself to the habit of daily--or constant and disciplined--coding-practice. 
+*_>> Mode of execution (of above purpose): simply copying--via typing, not copy-paste--the above minMaxAvg challenge from day 2.
+*/
+//?_1 (of 1):
 "use strict";

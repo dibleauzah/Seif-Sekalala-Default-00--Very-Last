4c8f9631ee6e7e941df88d4aa14ec74f3f270d68
Seif Sekalala <dibleauzah@gmail.com>
2023-06-28 00:00:20 -0400

    Day 5 algo practice.

--- continuousAlgo-Practice/algoPractice.js
+++ continuousAlgo-Practice/algoPractice.js
@@ -13,7 +13,6 @@
 ?_Console.log ([algoName]);
 */
 
-
 //!_Day 2 of Algo-Practice: June 16 (2023)--JS Version.
 
 // let myArray = [1, 2, 3, 4, 5];
@@ -26,9 +25,7 @@
 // 	if (myArray[i] > max) max = myArray[i];
 // 	avg += myArray[i];
 // }
-
 // avg /= myArray.length;
-
 // console.log("Hi! Please note that your min-# is : " + min);
 // console.log(" ; in addition, your max-# is : " + max);
 // console.log(" ; and finally, your average is : " + avg + " Thanks for visiting. Have an awesome day! :-)");
@@ -38,7 +35,6 @@
 //*-------------------------------------//
 
 //!_Day 2 of Algo-Practice: June 16 (2023)--JS Version.
-
 /* 
 ?_Day 3: June 18, 2023 
 ?_Goals/"parameters" for this day/session's practice: 
@@ -53,16 +49,37 @@
 // var min = myArray[0];
 // var max = myArray[0];
 // var avg = 0;
-
 // for (let i = 0; i < myArray.length; i++){
 //     if (myArray[i] < min) min = myArray[i];
 //     if (myArray[i] > max) max = myArray[i];
 //     avg += myArray[i];
 // }
 // avg /= myArray.length;
-
 // console.log("Hi! Please note that your min-# is : " + min);
 // console.log(" ; in addition, your max-# is : " + max);
 // console.log(" ; and finally, your average is : " + avg + " Thanks for visiting. Have an awesome day! :-)");
-
 //?_Testing suspended for this session.
+
+//*-------------------------------------//
+
+//?_Day 5: June 27, 2023 
+
+//?_Goals/"parameters" for this day/session's practice: 
+//*_>> 1)--Again, simply for re-acclimation purposes. Meaning, for me to re-acclimate myself to the habit of constant and disciplined coding-practice. 
+function basicSkillTest([arr]){
+    for (let i = 0; i <= arr.length; i++)
+            if (arr[i] => 5){
+                console.log("Yyiippeee! :-)");
+    }
+    return arr[i];
+}
+
+
+
+console.log(([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+//?_In case of any difficulties, trouble-shooting / corrections suspended, if the above print command doesn't work! 
+//?_         ''              ... trouble-shooting tb-continued later/asap.
+//!_ Update: In the end, curiousity got the better of me, and I tried troubleshooting after failing to get the expected result!
+//!_FTR, the expected should be: ... (see below) ...
+ 
+//!_Result: [1, 2, 3, 4, Yyiippeee! :-), Yyiippeee! :-) ...]
